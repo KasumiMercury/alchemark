4230803cf15000e29b18b2a9ef8f518a7a509beb
KasumiMercury <naoki_shira@outlook.jp>
2025-03-11 17:39:33 +0900

    feat: enhance block type detection logic
    To be treated as a normal paragraph if not followed by a space

--- parser.go
+++ parser.go
@@ -2,20 +2,56 @@ package main
 
 import (
 	"github.com/KasumiMercury/alchemark/token"
-	"strings"
 )
 
 func DetectBlockType(input string) []token.Token {
-	if strings.HasPrefix(input, "#") {
+	currentPos := 0
+	currentChar := input[currentPos]
+
+	if currentChar == '#' {
+		currentPos++
+		currentChar = input[currentPos]
+
+		for currentChar == '#' {
+			currentPos++
+			currentChar = input[currentPos]
+		}
+
+		if currentChar != ' ' {
+			return []token.Token{{Type: token.Paragraph}}
+		}
+
 		return []token.Token{{Type: token.Heading}}
 	}
 
-	if strings.HasPrefix(input, "```") {
-		return []token.Token{{Type: token.CodeBlock}}
+	if currentChar == '`' {
+		for i := 0; i < 2; i++ {
+			if currentChar != '`' {
+				return []token.Token{{Type: token.Paragraph}}
+			}
+
+			currentPos++
+			currentChar = input[currentPos]
+		}
+
+		return []token.Token{
+			{Type: token.CodeBlock},
+		}
 	}
 
-	if strings.HasPrefix(input, "---") {
-		return []token.Token{{Type: token.Horizontal}}
+	if currentChar == '-' {
+		for i := 0; i < 2; i++ {
+			if currentChar != '-' {
+				return []token.Token{{Type: token.Paragraph}}
+			}
+
+			currentPos++
+			currentChar = input[currentPos]
+		}
+
+		return []token.Token{
+			{Type: token.Horizontal},
+		}
 	}
 
 	return []token.Token{{Type: token.Paragraph}}

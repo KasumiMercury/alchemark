5ba8a8aa92af61b2734222f0abcf193a6fbc9379
KasumiMercury <naoki_shira@outlook.jp>
2025-03-11 17:48:17 +0900

    test: add shortage pattern cases

--- parser_test.go
+++ parser_test.go
@@ -79,6 +79,56 @@ func TestDetectBlockTypeNoSpace(t *testing.T) {
 			args: args{input: "#Heading"},
 			want: []token.Token{{Type: token.Paragraph}},
 		},
+		{
+			name: "Heading2 no space",
+			args: args{input: "##Heading"},
+			want: []token.Token{{Type: token.Paragraph}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := DetectBlockType(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DetectBlockType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDetectBlockTypeShortage(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		input string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []token.Token
+	}{
+		{
+			name: "CodeBlock shortage",
+			args: args{input: "`"},
+			want: []token.Token{{Type: token.Paragraph}},
+		},
+		{
+			name: "CodeBlock shortage2",
+			args: args{input: "``"},
+			want: []token.Token{{Type: token.Paragraph}},
+		},
+		{
+			name: "Horizontal shortage",
+			args: args{input: "-"},
+			want: []token.Token{{Type: token.Paragraph}},
+		},
+		{
+			name: "Horizontal shortage2",
+			args: args{input: "--"},
+			want: []token.Token{{Type: token.Paragraph}},
+		},
 	}
 
 	for _, tt := range tests {

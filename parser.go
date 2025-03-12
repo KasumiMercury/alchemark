b518f93b241043852dea8606ee1a3021725646bc
KasumiMercury <naoki_shira@outlook.jp>
2025-03-12 21:47:18 +0900

    refactor: block type detection into separate functions

--- parser.go
+++ parser.go
@@ -4,67 +4,58 @@ import (
 	"github.com/KasumiMercury/alchemark/token"
 )
 
-func DetectBlockType(input string) []token.Token {
-	currentPos := 0
-	currentChar := input[currentPos]
-
-	if currentChar == '#' {
-		currentPos++
-		currentChar = input[currentPos]
-
-		for currentChar == '#' {
-			currentPos++
-
-			if currentPos >= len(input) {
+func HeadingDetector(input string) []token.Token {
+	level := 0
+	for _, char := range input {
+		if char == '#' && level < 6 {
+			level++
+		} else {
+			if char == ' ' {
+				return []token.Token{{Type: token.Heading}}
+			} else {
 				return []token.Token{{Type: token.Paragraph}}
 			}
-			currentChar = input[currentPos]
-		}
-
-		if currentChar != ' ' {
-			return []token.Token{{Type: token.Paragraph}}
-		}
-
-		return []token.Token{{Type: token.Heading}}
-	}
-
-	if currentChar == '`' {
-		for i := 0; i < 2; i++ {
-			if currentChar != '`' {
-				return []token.Token{{Type: token.Paragraph}}
-			}
-
-			currentPos++
-
-			if currentPos >= len(input) {
-				return []token.Token{{Type: token.Paragraph}}
-			}
-			currentChar = input[currentPos]
-		}
-
-		return []token.Token{
-			{Type: token.CodeBlock},
-		}
-	}
-
-	if currentChar == '-' {
-		for i := 0; i < 2; i++ {
-			if currentChar != '-' {
-				return []token.Token{{Type: token.Paragraph}}
-			}
-
-			currentPos++
-
-			if currentPos >= len(input) {
-				return []token.Token{{Type: token.Paragraph}}
-			}
-			currentChar = input[currentPos]
-		}
-
-		return []token.Token{
-			{Type: token.Horizontal},
 		}
 	}
 
 	return []token.Token{{Type: token.Paragraph}}
+}
+
+func CodeBlockDetector(input string) []token.Token {
+	if len(input) < 3 {
+		return []token.Token{{Type: token.Paragraph}}
+	}
+
+	if input[0:3] == "```" {
+		return []token.Token{{Type: token.CodeBlock}}
+	}
+
+	return []token.Token{{Type: token.Paragraph}}
+}
+
+func HorizontalDetector(input string) []token.Token {
+	if len(input) < 3 {
+		return []token.Token{{Type: token.Paragraph}}
+	}
+
+	if input[0:3] == "---" {
+		return []token.Token{{Type: token.Horizontal}}
+	}
+
+	return []token.Token{{Type: token.Paragraph}}
+}
+
+func DetectBlockType(input string) []token.Token {
+	firstChar := input[0]
+
+	switch firstChar {
+	case '#':
+		return HeadingDetector(input)
+	case '`':
+		return CodeBlockDetector(input)
+	case '-':
+		return HorizontalDetector(input)
+	default:
+		return []token.Token{{Type: token.Paragraph}}
+	}
 }

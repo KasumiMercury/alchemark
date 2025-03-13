d9dd27c8c31ff56aeb8eb50d5be73356230410df
KasumiMercury <naoki_shira@outlook.jp>
2025-03-13 21:31:01 +0900

    feat: parse multiline string to block

--- parser.go
+++ parser.go
@@ -2,7 +2,53 @@ package main
 
 import (
 	"github.com/KasumiMercury/alchemark/token"
+	"strings"
+	"sync"
 )
+
+type Parser struct {
+	lines []string
+}
+
+func NewParser(text string) *Parser {
+	return &Parser{
+		lines: strings.Split(text, "\n"),
+	}
+}
+
+func (p *Parser) ParseToBlocks() []token.Token {
+	if len(p.lines) == 0 {
+		return nil
+	}
+
+	type lineData struct {
+		index int
+		token token.Token
+	}
+
+	lineDataCh := make(chan lineData, len(p.lines))
+	var wg sync.WaitGroup
+
+	for i, line := range p.lines {
+		wg.Add(1)
+		go func(index int, line string) {
+			defer wg.Done()
+			lineDataCh <- lineData{index, DetectBlockType(line)}
+		}(i, line)
+	}
+
+	go func() {
+		wg.Wait()
+		close(lineDataCh)
+	}()
+
+	var tokens []token.Token
+	for data := range lineDataCh {
+		tokens = append(tokens, data.token)
+	}
+
+	return tokens
+}
 
 func HeadingDetector(input []rune) (token.Token, bool) {
 	level := 0
--- parser_test.go
+++ parser_test.go
@@ -6,6 +6,38 @@ import (
 	"testing"
 )
 
+func TestParser_ParseToBlock(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input string
+		want  []token.Token
+	}{
+		{
+			input: "",
+			want:  nil,
+		},
+		{
+			input: "# Heading\nParagraph",
+			want: []token.Token{
+				token.NewHeadingBlock("Heading", 1),
+				token.NewParagraphBlock("Paragraph", 0),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
+
+			p := NewParser(tt.input)
+			if got := p.ParseToBlocks(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parser.ParseBlocks() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestCountIndent(t *testing.T) {
 	t.Parallel()
 

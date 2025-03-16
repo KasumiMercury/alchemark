494273aa3de14e46a1c107d055ef6a5b8cae785f
KasumiMercury <naoki_shira@outlook.jp>
2025-03-16 11:14:11 +0900

    fix: sort blocks before processing tokens in parser

--- parser.go
+++ parser.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/KasumiMercury/alchemark/token"
+	"sort"
 	"strings"
 	"sync"
 )
@@ -42,23 +43,33 @@ func (p *Parser) ParseToBlocks() []token.BlockToken {
 		close(lineDataCh)
 	}()
 
-	tokens := make([]token.BlockToken, 0, len(p.lines))
-
-	var openingCodeBlockFence *token.CodeBlockFence
-	codeBuffer := make([]string, 0)
+	blocks := make([]lineData, 0, len(p.lines))
 
 	for data := range lineDataCh {
 		if data.token == nil {
 			continue
 		}
 
-		if data.token.Type() == token.CodeBlockFenceType {
+		blocks = append(blocks, data)
+	}
+
+	sort.Slice(blocks, func(i, j int) bool {
+		return blocks[i].index < blocks[j].index
+	})
+
+	tokens := make([]token.BlockToken, 0, len(p.lines))
+
+	var openingCodeBlockFence *token.CodeBlockFence
+	codeBuffer := make([]string, 0)
+
+	for _, block := range blocks {
+		if block.token.Type() == token.CodeBlockFenceType {
 			if openingCodeBlockFence == nil {
-				openingCodeBlockFence = data.token.(*token.CodeBlockFence)
+				openingCodeBlockFence = block.token.(*token.CodeBlockFence)
 				continue
 			}
 
-			fenceToken := data.token.(*token.CodeBlockFence)
+			fenceToken := block.token.(*token.CodeBlockFence)
 			if fenceToken.InfoString() == "" && fenceToken.FenceChar() == openingCodeBlockFence.FenceChar() {
 				tokens = append(tokens, token.NewCodeBlock(openingCodeBlockFence.InfoString(), codeBuffer))
 				openingCodeBlockFence = nil
@@ -68,11 +79,11 @@ func (p *Parser) ParseToBlocks() []token.BlockToken {
 		}
 
 		if openingCodeBlockFence != nil {
-			codeBuffer = append(codeBuffer, p.lines[data.index])
+			codeBuffer = append(codeBuffer, p.lines[block.index])
 			continue
 		}
 
-		tokens = append(tokens, data.token)
+		tokens = append(tokens, block.token)
 	}
 
 	return tokens
